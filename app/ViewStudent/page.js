4ccc23b5dcd8d0597d2a19942d14cb468678bd0a
Vmkmanoj <vmkmanoj13@gmail.com>
2025-01-24 16:11:00 +0530

    page added

--- app/ViewStudent/page.js
+++ app/ViewStudent/page.js
@@ -13,7 +13,7 @@ export default function ViewStudent() {
   useEffect(() => {
     const fetchStudents = async () => {
       try {
-        const response = await fetch("http://127.0.0.1:5000/getUser");
+        const response = await fetch("https://backend-ozb3.vercel.app/getUser");
         if (!response.ok) {
           throw new Error(`Error: ${response.statusText}`);
         }
